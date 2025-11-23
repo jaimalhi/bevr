3e9b99174286636f25c91a600ecf62f6807c465b
Jai <malhi.jaivir@gmail.com>
2025-11-22 22:20:50 -0800

    profile: add vico bar chart for better visuals

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ android {
 
 dependencies {
     implementation(libs.androidx.room.runtime)
+    implementation(libs.compose)
+    implementation(libs.compose.m3)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.paging)
