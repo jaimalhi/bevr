c37cf2d0eaecb92ab9c4141ba99300e09c8f0dd6
Jai <malhi.jaivir@gmail.com>
2025-11-25 01:22:54 -0800

    create-webpage: address pr comments

--- docs/index.js
+++ docs/index.js
@@ -1,25 +1,2 @@
 const CURRENT_YEAR = new Date().getFullYear();
 document.getElementById("current-year").textContent = CURRENT_YEAR;
-
-const GITHUB_LINKS = [
-   {
-      name: "Jai Malhi",
-      url: "https://github.com/jaimalhi",
-   },
-   {
-      name: "Richard Gao",
-      url: "https://github.com/MrFlyingPizza",
-   },
-   {
-      name: "David Mulej",
-      url: null,
-   },
-   {
-      name: "Andrew La Grange",
-      url: null,
-   },
-   {
-      name: "Noah Zakaib",
-      url: null,
-   },
-];
